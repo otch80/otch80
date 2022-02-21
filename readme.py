11d5deb955dd825d9b9df50674edde9a53f87af6
otch80 <48629275+otch80@users.noreply.github.com>
2022-02-21 14:45:02 +0900

    Update readme.py

--- readme.py
+++ readme.py
@@ -3,20 +3,59 @@ import feedparser, datetime
 git_blog_rss_uri="https://otch80.github.io/feed.xml"
 rss_feed = feedparser.parse(git_blog_rss_uri)
 
-MAX_POST_NUM = 10
+MAX_POST_NUM = 5
 
-latest_blog_post_list = ""
+front_markdown_text = """
 
 
-for idx, feed in enumerate(rss_feed['entries']):
-    if idx > MAX_POST_NUM:
+<div align="center">
+
+
+# Hi, I'm MinGyu👋
+
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fotch80&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
+
+
+## Blog
+
+<a href="https://otch80.github.io/" target="_blank"><img src="https://img.shields.io/badge/otch's Blog-<COLOR CODE>?style=flat-square&logo=<ICON>&logoColor=white"/></a>
+
+### 🔥Latest Blog Post🔥
+"""
+
+back_markdown_text = """
+
+## Languages
+<img src="https://img.shields.io/badge/Python-3776AB?style=flat-square&logo=Python&logoColor=white"/> <img src="https://img.shields.io/badge/Node.js-339933?style=flat-square&logo=Node.js&logoColor=white"/> <img src="https://img.shields.io/badge/Spring Boot-6DB33F?style=flat-square&logo=Spring Boot&logoColor=white"/> <img src="https://img.shields.io/badge/java-007396?style=flat-square&logo=Spring Boot&logoColor=white"/> 
+
+<br>
+<br>
+  
+![trophy](https://github-profile-trophy.vercel.app/?username=otch80)
+
+<br>
+<br>
+
+[![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=otch80&theme=gruvbox)](https://github.com/anuraghazra/github-readme-stats) [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=otch80&layout=compact)](https://github.com/anuraghazra/github-readme-stats)
+
+</div>
+
+
+
+"""
+
+
+j=0
+for i, feed in enumerate(rss_feed['entries']):
+    if i > 5:
         break
-    feed_date = feed['published_parsed']
-    latest_blog_post_list += f"[{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
-    
-markdown_text = "##🔥🔥Latest Blog Post\n"
+    else:
+        dt = feed['published']
+        # markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
+        front_markdown_text += f"[{feed['title']}]({feed['link']}) <br>\n"
 
-readme_text = f"{markdown_text}{latest_blog_post_list}"
-
-with open("README.md", 'w', encoding='utf-8') as f:
-    f.write(readme_text)
+front_markdown_text += back_markdown_text
+print(front_markdown_text)
+f = open("README.md",mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()  
